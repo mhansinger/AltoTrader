63a0fa290d9f28780059e9307f7043fc88930967
mhansinger <maximilian.hansinger@gmail.com>
2017-05-22 21:45:58 +0200

    main_update

--- Trade_Algo/main.py
+++ Trade_Algo/main.py
@@ -5,7 +5,7 @@ This is the main file to run the trading bot
 '''
 
 from __init__ import *
-
+from datetime import datetime
 
 
 # set the input data with default values
@@ -26,3 +26,35 @@ XETH_trade = criteria(XETH_input,XETH_broker,XETH_history)
 def run(interval=600):
     XETH_trade.intersect()
     threading.Timer(interval, run).start()
+
+
+class intersect_thread(threading.Thread):
+    def __init__(self, asset, timeInterval=600):
+        threading.Thread.__init__(self)
+        self.iterations = 0
+        self.daemon = True  # OK for main to exit even if instance is still running
+        self.paused = True  # start out paused
+        self.state = threading.Condition()
+        self.__asset = asset
+        self.timeInteval = timeInterval
+        
+    def run(self):
+        self.resume() # unpause self
+        while True:
+            with self.state:
+                if self.paused:
+                    self.state.wait() # block until notified
+            self.__asset.intersect()
+            print('last intersect: ' + str(datetime.now()))
+            time.sleep(self.timeInteval)
+            self.iterations += 1
+
+    def resume(self):
+        with self.state:
+            self.paused = False
+            self.state.notify()  # unblock self if waiting
+
+    def pause(self):
+        with self.state:
+            self.paused = True  # make self block and wait
+            print('Strategy is currently paused!')
