4628ea7fcb95fc41c00e9df3fa4066f8e850a23f
mhansinger <maximilian.hansinger@gmail.com>
2017-05-22 22:16:10 +0200

    bla

--- Trade_Algo/__init__.py
+++ Trade_Algo/__init__.py
@@ -4,6 +4,7 @@ from Broker_virtual import Broker_virtual
 from set_input import set_input
 from history_data import history
 from criteria import criteria
+from run_strategy import run_strategy
 import threading
 import time
 from datetime import datetime
