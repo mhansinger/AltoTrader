9c2bab95f91487764726441913d7def670b3d8b3
mhansinger <maximilian.hansinger@gmail.com>
2017-09-10 18:45:15 +0200

    dynamicSMA

--- Kraken_Stream/DynamicSMA/dynamic_main.py
+++ Kraken_Stream/DynamicSMA/dynamic_main.py
@@ -4,19 +4,18 @@ import numpy as np
 from dynamicSMA import dynamicSMA
 import threading
 
-XXBTZUSD_dynamic = dynamicSMA('XXBT','ZUSD',length=14400,investment=1000.0, transaction_fee=0.0026)
+XXBTZUSD_dynamic = dynamicSMA('XXBT','ZUSD',length=28800,investment=1000.0, transaction_fee=0.002)
 
 # run twice a day: 43200 seconds
-
 
 def main(interval=43200):
     # introduce a bit of randomness in the parameter choice
     startlong = 750 + 5 * np.random.randint(10)
     stoplong = 1500
-    longinterval = 30 + 2 * np.random.randint(10)
+    longinterval = 50
     startshort = 250 + 5 * np.random.randint(10)
     stopshort = 650
-    shortinterval = 10 + np.random.randint(10)
+    shortinterval = 20
     try:
         XXBTZUSD_dynamic.optimizeSMA(startlong,stoplong,longinterval,startshort,stopshort,shortinterval)
         t = threading.Timer(interval, main)
